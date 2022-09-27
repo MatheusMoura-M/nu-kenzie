5a1f05d8355d66d4e419272a94347b0237be7d82
Matheus M <matheus150101miranda@gmail.com>
2022-09-27 16:58:44 -0300

    refatorei o cod, para troca de backgrounds no filtro das despesas

--- src/components/List/list.jsx
+++ src/components/List/list.jsx
@@ -2,39 +2,49 @@ import { useState } from "react";
 import { Card } from "../Card/card";
 import "./style.css";
 import carrinhoVazio from "../../../public/assets/NoCard.svg";
+import { BtnAll } from "./style";
 
 export const List = ({ listTransactions, setListTransactions }) => {
   const [filter, setFilter] = useState("todos");
+  const [btnAll, setBtnAll] = useState(false);
+  const [btnEntries, setBtnEntries] = useState(false);
+  const [btnExpenses, setBtnExpenses] = useState(false);
+
+  const btnBgc = (status) => {
+    const bgc = status ? "var(--color-primary)" : "var(--grey-6)";
+    return bgc;
+  };
+
+  const btnColor = (status) => {
+    const color = status ? "var(--grey-6)" : "var(--grey-2)";
+    return color;
+  };
 
   const handleTodos = (event) => {
     event.preventDefault();
-    const btns = event.target.parentElement.children;
 
-    event.target.classList.add("btn-color");
-    btns[1].classList.remove("btn-color");
-    btns[2].classList.remove("btn-color");
-
+    setBtnAll(true);
+    setBtnEntries(false);
+    setBtnExpenses(false);
     setFilter("todos");
   };
 
   const handleEntries = (event) => {
     event.preventDefault();
-    const btns = event.target.parentElement.children;
 
-    event.target.classList.add("btn-color");
-    btns[0].classList.remove("btn-color");
-    btns[2].classList.remove("btn-color");
+    setBtnEntries(true);
+    setBtnAll(false);
+    setBtnExpenses(false);
 
     setFilter("Entrada");
   };
 
   const handleExpenses = (event) => {
     event.preventDefault();
-    const btns = event.target.parentElement.children;
 
-    event.target.classList.add("btn-color");
-    btns[0].classList.remove("btn-color");
-    btns[1].classList.remove("btn-color");
+    setBtnExpenses(true);
+    setBtnAll(false);
+    setBtnEntries(false);
 
     setFilter("Despesa");
   };
@@ -44,9 +54,27 @@ export const List = ({ listTransactions, setListTransactions }) => {
       <div className="header_list">
         <h2>Resumo financeiro</h2>
         <div className="btn_filtro">
-          <button onClick={handleTodos}>Todos</button>
-          <button onClick={handleEntries}>Entradas</button>
-          <button onClick={handleExpenses}>Despesas</button>
+          <BtnAll
+            onClick={handleTodos}
+            bg={btnBgc(btnAll)}
+            color={btnColor(btnAll)}
+          >
+            Todos
+          </BtnAll>
+          <BtnAll
+            onClick={handleEntries}
+            bg={btnBgc(btnEntries)}
+            color={btnColor(btnEntries)}
+          >
+            Entradas
+          </BtnAll>
+          <BtnAll
+            onClick={handleExpenses}
+            bg={btnBgc(btnExpenses)}
+            color={btnColor(btnExpenses)}
+          >
+            Despesas
+          </BtnAll>
         </div>
       </div>
       {listTransactions.length !== 0 ? (
---
+++ src/components/List/style.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const BtnAll = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-family: var(--font-family);
+  font-size: var(--fontSize-text-2);
+  font-weight: var(--fontWei-title-4);
+  color: ${({ color }) => color};
+  width: 70px;
+  border-radius: 0.4rem;
+  background-color: ${({ bg }) => bg};
+  border: 1px solid transparent;
+  transition: box-shadow 0.3s ease, transform 0.3s ease, background-color 0.5s,
+    color 0.5s;
+
+  :hover {
+    border-color: var(--color-primary-2);
+    box-shadow: 2px 2px 0 0.5px #a01d4b;
+    background-color: var(--color-primary-2);
+    color: var(--grey-6);
+    transform: scale(1.08);
+  }
+
+  :active {
+    box-shadow: 0 0 0 0, inset 2px 2px 2px 0.5px #a01d4b;
+    transform: translate(2px, 2px);
+  }
+`;
